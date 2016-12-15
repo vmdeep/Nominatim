605d3baf14e7564f9cf0ac0f645139848b49b9aa
Home <vmdeep2@gmail.com>
2016-12-15 15:04:22 +0300

    some changes

--- settings/defaults.php
+++ settings/defaults.php
@@ -81,7 +81,7 @@ if (isset($_GET['debug']) && $_GET['debug']) @define('CONST_Debug', true);
 @define('CONST_Default_Lat', 20.0);
 @define('CONST_Default_Lon', 0.0);
 @define('CONST_Default_Zoom', 2);
-@define('CONST_Map_Tile_URL', 'http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+@define('CONST_Map_Tile_URL', 'http://37.204.132.203:8083/osm_tiles/{z}/{x}/{y}.png');
 @define('CONST_Map_Tile_Attribution', ''); // Set if tile source isn't osm.org
 
 @define('CONST_Search_AreaPolygons', true);
